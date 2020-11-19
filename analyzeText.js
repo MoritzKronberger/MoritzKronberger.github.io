be2152b46283ea1180fe28223b0b6fa3a5ef6adf
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2020-11-19 14:31:19 +0100

    add textanalyzing functions

--- analyzeText.js
+++ analyzeText.js
@@ -1,3 +1,49 @@
+words = [];
+wordset = new Set();
+let songtext = '';
+countedWords = {};
+let rankedWords;
+
 function preload(){
-    sontext = loadStrings('songtext.txt');
+    st = loadStrings('songtext.txt');
+}
+
+function setup(){
+
+    createCanvas(800,800);
+
+    console.log(st);
+    for(let i = 0; i<st.length; i++){
+        songtext = songtext + ' ' + st[i];
+    }
+    console.log(songtext);
+    words = songtext.split(' ');
+    console.log(words);
+
+    for( i=0; i< words.length; i++){
+        wordset.add(words[i]);
+    }
+    console.log(wordset);
+
+    wordset.forEach(function(e){
+        let count = 0;
+        for(let i =0; i< words.length; i++){
+            if(words[i]==e){
+                count++;
+            }
+        }
+        countedWords[e]=count;
+    });
+    console.log(countedWords);
+    rankedWords = sortWords(countedWords);
+    console.log(rankedWords);
+
+    for(i=0; i<10; i++){
+        text(rankedWords[i], 20, (i+1)*50);
+    }
+}
+
+function sortWords(obj) {
+    var keys = keys = Object.keys(obj);
+    return keys.sort(function(a,b){return obj[b]-obj[a]});
 }
